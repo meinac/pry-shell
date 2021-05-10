71a3ffc186c7e1668a0d08cad9af63d50a16f8eb
Mehmet Emin INAC <mehmetemininac@gmail.com>
2021-05-10 22:59:20 +0300

    Send `whereami` to session on disconnect

--- lib/pry/shell/command.rb
+++ lib/pry/shell/command.rb
@@ -5,13 +5,14 @@ class Pry
     class Command
       EXIT_COMMANDS = %w[exit !!!].freeze
       DISC_COMMAND = "\\m"
+      DISC_FALLBACK = "whereami"
 
       def self.execute(client, string)
         case string
         when *EXIT_COMMANDS
           Shell.registry.remove(client) && string
         when DISC_COMMAND
-          Shell.registry.disconnect(client) && ""
+          Shell.registry.disconnect(client) && DISC_FALLBACK
         else
           string
         end
