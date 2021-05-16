4e346b58112108bbc797a6429b24eddf5396cc64
Mehmet Emin INAC <mehmetemininac@gmail.com>
2021-05-16 09:20:17 +0300

    Update the about ui

--- lib/pry/shell/ui/about.rb
+++ lib/pry/shell/ui/about.rb
@@ -9,7 +9,7 @@ class Pry
           pry-shell version "#{VERSION}"
 
           Pry-shell provides you a standalone shell for accessing multiple `pry` sessions running on different processes.
-          You can switch between sessions by using the "\\h" command.
+          You can switch between sessions by going back to the menu using the "\\m" command.
 
           Written by Mehmet Emin INAC.
         MARKDOWN
