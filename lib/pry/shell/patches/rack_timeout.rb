359be33a6bb5917ebc3a34b96ed71d5833425348
Mehmet Emin INAC <mehmetemininac@gmail.com>
2023-08-02 12:19:21 +0200

    Fix rack-timeout patch

--- lib/pry/shell/patches/rack_timeout.rb
+++ lib/pry/shell/patches/rack_timeout.rb
@@ -5,7 +5,9 @@ class Pry
     module Patches
       module RackTimeout
         def initialize(&on_timeout)
-          @on_timeout = -> (thread) { Shell.active_shell_options(thread: thread) || on_timeout || ON_TIMEOUT }
+          timeout_handler = on_timeout || ON_TIMEOUT
+
+          @on_timeout = -> (thread) { Shell.active_shell_options(thread: thread) || timeout_handler.call(thread) } # Do not raise timeout error if the shell session is active
           @scheduler  = Rack::Timeout::Scheduler.singleton
         end
       end
